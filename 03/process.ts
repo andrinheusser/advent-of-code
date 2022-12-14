92f58717fe6bb00853ba85e423794f42d2137b29
Andrin Heusser <andrinheusser@gmail.com>
2022-12-14 23:22:10 +0100

    looks smarter

--- 03/process.ts
+++ 03/process.ts
@@ -1,31 +1,43 @@
 const input = await Deno.readTextFile("input.txt");
-
 const rucksacks = input.split("\r\n").filter((l) => !!l);
 
-function alphabetPosition(text: string) {
-  return [...text].map((a) =>
-    a === a.toLowerCase() ? parseInt(a, 36) - 10 : parseInt(a, 36) - 10 + 26
-  ).filter((a) => a >= 0).map((a) => a + 1);
+function priorityLevel(text: string) {
+  return [...text]
+    .map((a) =>
+      a === a.toLowerCase() ? parseInt(a, 36) - 10 : parseInt(a, 36) - 10 + 26
+    )
+    .filter((a) => a >= 0)
+    .map((a) => a + 1);
 }
 
-const groups: Array<Array<string>> = [];
+const part1 = () =>
+  rucksacks
+    .reduce<Array<number>>((acc, r) => {
+      const comp2 = r.slice(r.length / 2);
+      return [
+        ...acc,
+        ...new Set(
+          priorityLevel(
+            [...r.slice(0, r.length / 2)]
+              .filter((a) => comp2.includes(a))
+              .join("")
+          )
+        ),
+      ];
+    }, [])
+    .reduce((a, b) => a + b, 0);
 
-// group the rucksacks in groups of three
-for (let i = 0; i < rucksacks.length; i++) {
-  if (i % 3 === 0) {
-    groups.push([rucksacks[i], rucksacks[i + 1], rucksacks[i + 2]]);
-  }
-}
+const part2 = () =>
+  rucksacks.reduce<number>((acc, _r, i, arr) => {
+    return i % 3 === 0
+      ? acc +
+          priorityLevel(
+            [...arr[i]].find(
+              (a) => arr[i + 1].includes(a) && arr[i + 2].includes(a)
+            )!
+          )[0]
+      : acc;
+  }, 0);
 
-const badges = groups.map((group) => {
-  const [first, second, third] = group;
-  for (let c of first) {
-    if (second.includes(c) && third.includes(c)) {
-      return c;
-    }
-  }
-  return "";
-});
-
-console.log(badges.map(alphabetPosition).flat().reduce((a, b) => a + b, 0));
-console.log(badges.length);
+console.log(part1());
+console.log(part2());
