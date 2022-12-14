24c060d67741095d9c0c6337ca4284cf1bf667b7
Andrin Heusser <andrinheusser@gmail.com>
2022-12-14 22:05:50 +0100

    its probably faster this way

--- 01/process.ts
+++ 01/process.ts
@@ -3,19 +3,33 @@ const input = await Deno.readTextFile("input.txt");
 //const lines = input.split("\r\n").filter((l) => !!l);
 const lines = input.split("\r\n");
 
-const calories: Map<number, number> = new Map();
+// part 1
+const part1 = () =>
+  lines
+    .reduce<[number, number]>(
+      (acc, line) =>
+        line
+          ? [acc[0] + parseInt(line), acc[1]]
+          : acc[0] > acc[1]
+          ? [0, acc[0]]
+          : [0, acc[1]],
+      [0, 0]
+    )
+    .pop();
 
-let i = 0;
-for (let line of lines) {
-  const iCal = calories.get(i);
-  if (line) {
-    calories.set(i, iCal ? iCal + parseInt(line) : parseInt(line));
-  } else {
-    i++;
-  }
-}
-const sorted = [...calories.values()].sort((a, b) => a - b).reverse();
+// part 2
+const part2 = () =>
+  lines
+    .reduce<[number, Array<number>]>(
+      (acc, line) =>
+        line
+          ? [acc[0] + parseInt(line), acc[1]]
+          : acc[0] > acc[1][2]
+          ? [0, [acc[1][0], acc[1][1], acc[0]].sort((a, b) => b - a)]
+          : [0, acc[1]],
+      [0, [0, 0, 0]]
+    )[1]
+    .reduce((a, b) => a + b);
 
-const topThree = sorted.slice(0, 3).reduce((a, b) => a + b, 0);
-
-console.log({ sorted: sorted.slice(0, 3), topThree });
+console.log("part 1", part1());
+console.log("part 2", part2());
