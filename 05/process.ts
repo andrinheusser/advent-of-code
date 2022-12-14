1b221983a6d3b037f1745f526fabd093b5fdfca9
Andrin Heusser <andrinheusser@gmail.com>
2022-12-15 00:46:40 +0100

    parse stacks from file

--- 05/process.ts
+++ 05/process.ts
@@ -1,19 +1,10 @@
-const input = await Deno.readTextFile("input.txt");
-
+const input = Deno.readTextFileSync("./input.txt");
 const instructions = input.split("\r\n").filter((l) => !!l);
-
-const parseInstruction = (instruction: string) => {
-  const [_, count, from, to] =
-    instruction.match(/move (\d+) from (\d+) to (\d+)/) ?? [];
-  return {
-    count: parseInt(count),
-    from: parseInt(from),
-    to: parseInt(to),
-  };
-};
+const stackInput = await Deno.readTextFile("./stacks.txt");
+const stackLines = stackInput.split("\n").filter(Boolean).slice(1).reverse();
 
 class Stack {
-  constructor(private crates: string[]) {}
+  constructor(public crates: string[]) {}
   take(count: number) {
     return this.crates.splice(this.crates.length - count, count);
   }
@@ -21,45 +12,62 @@ class Stack {
     return this.crates[this.crates.length - 1];
   }
   add(crates: string[]) {
-    this.crates.push(...crates);
-  }
-  isEmpty() {
-    return this.crates.length === 0;
+    this.crates.push(...crates.filter((c) => c !== " "));
   }
 }
 
-/***
- * Stacks
-    [B]             [B] [S]
-    [M]             [P] [L] [B] [J]
-    [D]     [R]     [V] [D] [Q] [D]
-    [T] [R] [Z]     [H] [H] [G] [C]
-    [P] [W] [J] [B] [J] [F] [J] [S]
-[N] [S] [Z] [V] [M] [N] [Z] [F] [M]
-[W] [Z] [H] [D] [H] [G] [Q] [S] [W]
-[B] [L] [Q] [W] [S] [L] [J] [W] [Z]
- 1   2   3   4   5   6   7   8   9
- */
-
-const stacks = [
-  new Stack(["B", "W", "N"]),
-  new Stack(["L", "Z", "S", "P", "T", "D", "M", "B"]),
-  new Stack(["Q", "H", "Z", "W", "R"]),
-  new Stack(["W", "D", "V", "J", "Z", "R"]),
-  new Stack(["S", "H", "M", "B"]),
-  new Stack(["L", "G", "N", "J", "H", "V", "P", "B"]),
-  new Stack(["J", "Q", "Z", "F", "H", "D", "L", "S"]),
-  new Stack(["W", "S", "F", "J", "G", "Q", "B"]),
-  new Stack(["Z", "W", "M", "S", "C", "D", "J"]),
-];
-
-for (let i = 0; i < instructions.length; i++) {
-  const { count, from, to } = parseInstruction(instructions[i]);
-  const fromStack = stacks[from - 1];
-  const toStack = stacks[to - 1];
-  const crates = fromStack.take(count);
-  toStack.add(crates);
+function* crateFromStackInput(lines: string[]) {
+  for (const line of lines) {
+    let stackIndex = -1;
+    for (let i = 1; i < line.length; i += 4) {
+      yield [++stackIndex, line[i]];
+    }
+  }
 }
 
-const topCrates = stacks.map((s) => s.peek()).join("");
-console.log(topCrates);
+function* commands(instructions: string[]) {
+  for (const instruction of instructions) {
+    const [_, count, from, to] =
+      instruction.match(/move (\d+) from (\d+) to (\d+)/) ?? [];
+    yield {
+      count: parseInt(count),
+      from: parseInt(from),
+      to: parseInt(to),
+    };
+  }
+}
+
+function getStacks(stackLines: string[]): Array<Stack> {
+  const stacks: Array<Stack> = [];
+  for (const [stackIndex, crate] of crateFromStackInput(
+    stackLines
+  ) as Generator<[number, string]>) {
+    if (!stacks[stackIndex]) {
+      stacks[stackIndex] = new Stack([crate]);
+    } else {
+      stacks[stackIndex].add([crate]);
+    }
+  }
+  return stacks;
+}
+
+export const part1 = () => {
+  const stacks = getStacks(stackLines);
+  for (const { count, from, to } of commands(instructions)) {
+    for (let i = 0; i < count; i++) {
+      stacks[to - 1].add(stacks[from - 1].take(1));
+    }
+  }
+  return stacks.map((s) => s.peek()).join("");
+};
+
+export const part2 = () => {
+  const stacks = getStacks(stackLines);
+  for (const { count, from, to } of commands(instructions)) {
+    stacks[to - 1].add(stacks[from - 1].take(count));
+  }
+  return stacks.map((s) => s.peek()).join("");
+};
+
+console.log("part 1: " + part1());
+console.log("part 2: " + part2());
