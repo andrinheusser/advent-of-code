261771b9d8ba1332305c1d9d3e233c1c52fa658e
Andrin Heusser <andrinheusser@gmail.com>
2022-12-14 23:38:34 +0100

    if all you have is Array.reduce...

--- 04/process.ts
+++ 04/process.ts
@@ -2,34 +2,32 @@ const input = await Deno.readTextFile("input.txt");
 
 const lines = input.split("\r\n").filter((l) => !!l);
 
-const pairs: number[][][] = [];
+const parsePairs = (line: string) =>
+  line.split(",").map((a) => a.split("-").map(Number));
 
-for (let i = 0; i < lines.length; i++) {
-  pairs.push(lines[i].split(",").map((a) => a.split("-").map(Number)));
-}
+type Range = [number, number];
 
-const _hasFullOverlap = (pair: number[][]) => {
-  const [a, b] = pair;
-  return a[0] <= b[0] && a[1] >= b[1] || b[0] <= a[0] && b[1] >= a[1];
-};
+const isWithin = (value: number, range: Range) =>
+  value >= range[0] && value <= range[1];
+const isFullyContained = (pair: Range, range: Range) =>
+  isWithin(pair[0], range) && isWithin(pair[1], range);
 
-const hasPartialOverlap = (pair: number[][]) => {
-  const [a, b] = pair;
-  const [a1, a2] = a;
-  const [b1, b2] = b;
-  // return true if a1 is between b1 and b2
-  // or if a2 is between b1 and b2
-  // or if b1 is between a1 and a2
-  // or if b2 is between a1 and a2
-  return (a1 >= b1 && a1 <= b2) || (a2 >= b1 && a2 <= b2) ||
-    (b1 >= a1 && b1 <= a2) || (b2 >= a1 && b2 <= a2);
-};
+const part1 = () =>
+  lines.reduce((acc, line) => {
+    const [a, b] = parsePairs(line) as [Range, Range];
+    return isFullyContained(a, b) || isFullyContained(b, a) ? acc + 1 : acc;
+  }, 0);
 
-let z = 0;
-for (let i = 0; i < pairs.length; i++) {
-  const pair = pairs[i];
-  if (hasPartialOverlap(pair)) {
-    z++;
-  }
-}
-console.log(z);
+const part2 = () =>
+  lines.reduce((acc, line) => {
+    const [a, b] = parsePairs(line) as [Range, Range];
+    return isWithin(a[0], b as Range) ||
+      isWithin(a[1], b as Range) ||
+      isWithin(b[0], a as Range) ||
+      isWithin(b[1], a as Range)
+      ? acc + 1
+      : acc;
+  }, 0);
+
+console.log(part1());
+console.log(part2());
